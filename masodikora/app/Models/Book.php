3db20477408ef35e190cfb649be3524ad7f3b999
galpeter94 <galpeter94@gmail.com>
2025-10-01 19:47:41 +0200

    KEZS

--- masodikora/app/Models/Book.php
+++ masodikora/app/Models/Book.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-     protected $fillable = [
+    use HasFactory;
+
+    protected $fillable = [
         'author',
         'title',
         'pieces'
